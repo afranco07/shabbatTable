3cc55c299f1840a968c755b312301447730aa4e4
jnthnrzr <jonathanrozario@gmail.com>
2016-11-09 19:56:58 -0500

    Added Event"

--- frijay/admin.py
+++ frijay/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from frijay.models import User
+from frijay.models import Event
+
+
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Event)

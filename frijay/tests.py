60ad120b794063e6b520fe3e632125a73097f4a7
jnthnrzr <jonathanrozario@gmail.com>
2016-11-09 22:44:34 -0500

    tweak parameters

--- frijay/tests.py
+++ frijay/tests.py
@@ -7,15 +7,13 @@ from .models import User
 class EventModelTest(TestCase):
     '''Testing the Event Model'''
     def test_string_representation(self):
-        event = Event(title="Insert title")
+        '''Test __str__ method for Event'''
+        event = Event(title="My title")
         self.assertEqual(str(event), event.title)
 
 class UserModelTest(TestCase):
     '''Testing the User Model'''
     def test_string_representation(self):
-        user = User(first_name='Abham',last_name='Lin')
+        '''Test __str__ method for User'''
+        user = User(first_name='Abraham',last_name='Lincoln')
         self.assertEqual(str(user), user.first_name+user.last_name)
-
-    def test_first_name(self):
-        user = User(first_name='Fname')
-        self.assertEqual(str(user), user.first_name)

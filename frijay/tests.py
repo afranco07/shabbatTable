59b7e85d32c7dfdbd6b51dfb88b431e3e033a713
jnthnrzr <jonathanrozario@gmail.com>
2016-11-09 19:31:33 -0500

    Add test for Event

--- frijay/tests.py
+++ frijay/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Event
 
 # Create your tests here.
+class EventModelTest(TestCase):
+
+    def test_string_representation(self):
+        event = Event(title="My event title")
+        self.assertEqual(str(event), event.title)

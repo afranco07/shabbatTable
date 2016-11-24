1a68d33e85886dba220a909058c57027b65429f6
jnthnrzr <jonathanrozario@gmail.com>
2016-11-24 14:07:09 -0500

    Added userprofile

--- frijay/forms.py
+++ frijay/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.contrib.auth.models import User
+from frijay.models import UserProfile
 
 
 class UserForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput())
-class Meta:
-    model = User
-    fields = ('email', 'password')
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password')
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('picture',)

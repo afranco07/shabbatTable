6204dd1f20ddfdcd1ebdb1fa3e41181697044d60
jnthnrzr <jonathanrozario@gmail.com>
2016-11-24 16:52:17 -0500

    User is from django code

--- frijay/models.py
+++ frijay/models.py
@@ -20,7 +20,7 @@ class Event(models.Model):
     created_at = models.DateTimeField(auto_now_add=True, editable=False)
     modified_at = models.DateTimeField(auto_now=True, editable=False)
     address =  models.CharField(max_length=200, null=True)
-    host = models.ForeignKey('User', on_delete=models.CASCADE, null=True)
+    host = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     date = models.DateField(null=True)
     time = models.TimeField(null=True)
     openSeats = models.IntegerField(null=True)

8b9cac65742f1b7cde00d2bc97f144614545b6b1
jnthnrzr <jonathanrozario@gmail.com>
2016-11-24 16:12:39 -0500

    Added user_login()

--- frijay/views.py
+++ frijay/views.py
@@ -1,7 +1,10 @@
 '''Views for the frijay app'''
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.core.urlresolvers import reverse
+from django.http import HttpResponseRedirect, HttpResponse
 from frijay.forms import UserForm, UserProfileForm
-from django.http import HttpResponse
+
 
 def index(request):
     '''index page view
@@ -83,10 +86,40 @@ def signup(request):
                    'registered': registered})
 
 
-def login(request):
+def user_login(request):
     '''login page view'''
-    context_dict = {'title': "Login"}
-    return render(request, 'frijay/login.html', context_dict)
+    if request.method == 'POST':
+        # Gather the username and password provided by the user.
+        # This information is obtained from the login form.
+        # We use request.POST.get('<variable>') as opposed
+        # to request.POST['<variable>'], because the
+        # request.POST.get('<variable>') returns None if the
+        # value does not exist, while request.POST['<variable>'] # will raise a KeyError exception.
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        # Use Django's machinery to attempt to see if the username/password # combination is valid - a User object is returned if it is.
+        user = authenticate(username=username, password=password)
+        # If we have a User object, the details are correct.
+        # If None, no user # with matching credentials was found.
+        if user:
+            # Is the account active? It could have been disabled.
+            if user.is_active:
+                # If the account is valid and active, we can log the user in. # We'll send the user back to the homepage.
+                login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+            else:
+            # An inactive account was used - no logging in!
+                return HttpResponse("Your Rango account is disabled.")
+        else:
+            # Bad login details were provided. So we can't log the user in.
+            print("Invalid login details: {0}, {1}".format(username, password))
+            return HttpResponse("Invalid login details supplied.")
+        # The request is not a HTTP POST, so display the login form. # This scenario would most likely be a HTTP GET.
+    else:
+        # No context variables to pass to the template system, hence the # blank dictionary object...
+        return render(request, 'rango/login.html', {})
+            # context_dict = {'title': "Login"}
+            # return render(request, 'frijay/login.html', context_dict)
 
 
 def profile(request):

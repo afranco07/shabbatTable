d3977c1a1594a0c02912caa3ffc7fe4958a352b8
Avigdor <avigdorkirschenbaum@gmail.com>
2016-11-25 18:32:23 -0500

    populating event page

--- frijay/views.py
+++ frijay/views.py
@@ -135,39 +135,9 @@ def redir(request):
 
 def events(request):
     '''Event View'''
-    '''
-    all_events = Event.objects.all()
-    html = ''
-
-    for event in all_events:
-        ttl = event.address
-        url = '/frijay/events/' + str(event.id) + '/'
-        html += '<a href="' + url + '">' + event.title + '</a><br>'    
-    context_dict = {'html_list' : html}
+    all_events = Event.objects.all()   
+    context_dict = {'html_list' : all_events}
     return render(request, 'frijay/events.html', context_dict)
-    '''
-    ttl = ''
-    addr = ''
-    hst = ''
-    dt = ''
-    tm = ''
-    os = ''
-    all_events = Event.objects.all()
-    for x in all_events:
-        ttl += str(x.title)
-        addr += str(x.address)
-        hst += str(x.host)
-        dt += str(x.date)
-        tm += str(x.time)
-        os += str(x.openSeats)
-        
-    context_dict = {'title' : ttl,
-                    'address' : addr,
-                    'host' : hst,
-                    'date' : dt,
-                    'time' : tm,
-                    'openSeat' :os
-                    }
 
     return render(request, 'frijay/events.html', context_dict)
 @login_required

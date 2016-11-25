5a9839a016ba6fbba94abb41d8286ea6006602a0
Alberto Franco <afranco07@citymail.cuny.edu>
2016-11-25 02:12:25 -0500

    All the events will now show on the events page. Clicking on an event will show you the details of the event

--- frijay/views.py
+++ frijay/views.py
@@ -2,6 +2,8 @@
 
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Event
+from django.template import loader
 
 
 def index(request):
@@ -47,10 +49,27 @@ def redir(request):
     return redirect('/frijay')
 
 def events(request):
-    '''Reservations View'''
-    return render(request, 'frijay/events.html')
+    '''Event View'''
+    all_events = Event.objects.all()
+    html = ''
+
+    for event in all_events:
+        url = '/frijay/events/' + str(event.id) + '/'
+        html += '<a href="' + url + '">' + event.title + '</a><br>'
+    context_dict = {'html_list' : html}
+
+    return render(request, 'frijay/events.html', context_dict)
 
 def reservationsEvent(request, event_id):
-    '''Reservations view for specific event'''
-    context_dict = {'event_id' : event_id}
+    '''Event view for specific event'''
+    eventModel = Event.objects.get(id = event_id)
+    context_dict = {'event_id' : event_id,
+                    'event_title' : eventModel.title,
+                    'event_host' : eventModel.host,
+                    'event_address' : eventModel.address,
+                    'event_date' : eventModel.date,
+                    'event_time' : eventModel.time,
+                    'event_seats' : eventModel.openSeats,
+                    'event_details' : eventModel.additionalDetails
+                    }
     return render(request, 'frijay/reservationEventPage.html',context_dict)

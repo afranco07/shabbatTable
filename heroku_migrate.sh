69e2e7b84961ed307848060b65ff78c6b8e70db3
cartoonman <carl.colena@gmail.com>
2016-11-26 15:07:35 -0500

    fixing typo in heroku script. -Cartoonman

--- heroku_migrate.sh
+++ heroku_migrate.sh
@@ -1,2 +1,2 @@
 python manage.py migrate --settings=shabbattable.settings.prod
-python manage.py populate_db.py --settings=shabbattable.settings.prod
+python manage.py populate_db --settings=shabbattable.settings.prod

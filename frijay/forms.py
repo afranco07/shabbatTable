42551ca9bdb799fb6346a1589f16ff77446fb70d
jnthnrzr <jonathanrozario@gmail.com>
2016-11-30 02:21:57 -0500

    Added phone field

--- frijay/forms.py
+++ frijay/forms.py
@@ -54,30 +54,33 @@ class UserProfileForm(forms.ModelForm):
         fields = ('picture',)
 
 class EventForm(forms.ModelForm):
-    """ Form to Host a Dinner """
-    # title = forms.CharField(required=True,
-    #                         max_length=128,
-    #                         help_text="Please enter a title.")
+    """ Form to Host a Dinner. Host name selected in the views. """
     title = forms.CharField(
         widget=forms.TextInput(attrs={'required':True, 'max_length':30}))
     # Street Address, Line 1
-    address = forms.CharField(required=True,
+    address = forms.CharField(label="Street Address",
+                              required=True,
                               max_length=160,
                               help_text="Please enter the address.")
     # Street Address, Line 2
-    address2 = forms.CharField(max_length=40)
-    city = forms.CharField(required=True, max_length=15)
-    state = forms.CharField(required=True, max_length=15)
-    zipcode = forms.IntegerField(min_value=0, max_value=99999)
-    phone = forms.IntegerField(min_value=0, max_value=9999999)
-    date = forms.DateField(initial=datetime.today().date() + timedelta(days=7))
+    address2 = forms.CharField(label="Apt / Floor", max_length=40)
+    city = forms.CharField(label="City", required=True, max_length=15)
+    state = forms.CharField(label="State", required=True, max_length=15)
+    zipcode = forms.IntegerField(label="ZIP code",
+                                 min_value=10000,
+                                 max_value=99999)
+    phone = forms.IntegerField(min_value=1000000000, max_value=9999999999)
+    date = forms.DateField(label="Date", initial=datetime.today().date() + timedelta(days=7))
     # Starting time
-    time1 = forms.TimeField(initial=datetime.now().time())
+    time1 = forms.TimeField(label="Starting at", initial=datetime.now().time())
     # Ending time
-    time2 = forms.TimeField(initial=datetime.now().time())
-    openSeats = forms.IntegerField(
-        initial=1, validators=[MinValueValidator(1), MaxValueValidator(10)])
-    additionalDetails = forms.CharField(max_length=160)
+    time2 = forms.TimeField(label="Ending at", initial=datetime.now().time())
+    openSeats = forms.IntegerField(label="Available Seats",
+        initial=1, min_value=1, max_value=10)
+    additionalDetails = forms.CharField(
+        label="Details",
+        widget=forms.Textarea(attrs=dict(max_length=160)))
+
     class Meta:
         model = Event
         fields = ('title',
@@ -86,13 +89,9 @@ class EventForm(forms.ModelForm):
                   'city',
                   'state',
                   'zipcode',
+                  'phone',
                   'date',
                   'time1',
                   'time2',
                   'openSeats',
                   'additionalDetails',)
-    # def __init__(self, *args, **kwargs):
-    #     self.user = kwargs.pop('user', None)
-    #     super(EventForm, self).__init__(*args, **kwargs)
-    # def clean_username(self):
-    #     return self.user.username

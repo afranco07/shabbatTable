80d9593945470c51b5148eab800deea8676890e4
cartoonman <carl.colena@gmail.com>
2016-12-01 12:48:38 -0500

    adjusadjusting production settings for forcing ssl

--- shabbattable/settings/prod.py
+++ shabbattable/settings/prod.py
@@ -3,3 +3,7 @@ DEBUG = True
 #The following setting is for Heroku setup for Postgres.
 import dj_database_url
 DATABASES['default'] =  dj_database_url.config()
+
+SECURE_SSL_REDIRECT = True
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+

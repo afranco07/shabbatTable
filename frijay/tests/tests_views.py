9e20a19a66369aa500c137db8534e44e2922d722
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2016-12-04 16:02:36 -0500

    Moved tests to tests/ directory

--- frijay/tests/tests_views.py
+++ frijay/tests/tests_views.py
@@ -1,28 +1,3 @@
-'''Unittests for the frijay app'''
-from django.test import TestCase
-from .models import Event
-from .models import User
-
-
-# Create your tests here.
-class EventModelTest(TestCase):
-    '''Testing the Event Model'''
-
-    def test_string_representation(self):
-        '''Test __str__ method for Event'''
-        event = Event(title="My title")
-        self.assertEqual(str(event), event.title)
-
-
-class UserProfileModelTest(TestCase):
-    '''Testing the User Model'''
-
-    def test_string_representation(self):
-        '''Test __str__ method for User'''
-        userprofile = User(first_name='Abraham', last_name='Lincoln', username='mrpresident')
-        self.assertEqual(str(userprofile), userprofile.username)
-
-
 # class ViewsTest(TestCase):
 #     '''Test the index.html page'''
 #

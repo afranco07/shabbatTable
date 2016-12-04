64b48af1cafbf3f6812b3efbc328a51accc40b26
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2016-12-04 17:06:58 -0500

    Models Tests are now separate. Resolves #58

--- frijay/tests/tests_models.py
+++ frijay/tests/tests_models.py
@@ -12,13 +12,3 @@ class EventModelTest(TestCase):
         '''Test __str__ method for Event'''
         event = Event(title="My title")
         self.assertEqual(str(event), event.title)
-
-
-# class UserProfileModelTest(TestCase):
-#     '''Testing the User Model'''
-#
-#     def test_string_representation(self):
-#         '''Test __str__ method for User'''
-#         user = User(first_name='Abraham', last_name='Lincoln', username='mrpresident')
-#         userprofile = UserProfile(user=user)
-#         self.assertEqual(str(userprofile), userprofile.username)

b168151fcd4f2ff01f72277a1b73c038002487eb
Jonathan Rozario <jrozari00@citymail.cuny.edu>
2016-12-04 16:51:37 -0500

    Cleaned for pylint

--- frijay/admin.py
+++ frijay/admin.py
@@ -1,12 +1,9 @@
 '''Models are registered here'''
 from django.contrib import admin
-from frijay.models import Event
-from frijay.models import User
-# from frijay.models import UserProfile
+from frijay.models import Event, User
 
 
 # Register your models here.
 admin.site.unregister(User)
 admin.site.register(User)
-# admin.site.register(UserProfile)
 admin.site.register(Event)

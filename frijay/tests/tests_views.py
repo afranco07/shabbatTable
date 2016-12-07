86fcac512b24f8016c73201ac60e4d8e432e63b0
Ayaz Shah <ayazshah_20009@yahoo.com>
2016-12-07 03:42:46 -0500

    Tests for events shown on Homepage

--- frijay/tests/tests_views.py
+++ frijay/tests/tests_views.py
@@ -1,4 +1,6 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
+from frijay.models import Event
 
 class ViewsTest(TestCase):
 
@@ -37,3 +39,30 @@ class ViewsTest(TestCase):
         """Tests the signup page"""
         response = self.client.get("/signup/")
         self.assertEqual(response.status_code, 200)
+
+
+    """Test whether our events show up on the homepage"""
+
+    def setUp(self):
+        self.user = get_user_model().objects.create(username='some_user')
+
+    def test_one_event(self):
+        Event.objects.create(title='Shabbat',address='testaddress',city='Brooklyn',state='New York',
+                             phone='1231231234',date='2016-12-07',time1='03:24:12',time2="03:24:12",
+                             openSeats='10',additionalDetails='None')
+        response = self.client.get('/')
+        self.assertContains(response, 'Shabbat')
+        self.assertContains(response, 'Brooklyn')
+        self.assertContains(response, '10')
+
+    def test_two_events(self):
+        Event.objects.create(title='Frijay', address='testaddress2', city='Queens', state='New York',
+                             phone='1233213221', date='2016-12-07', time1='03:24:12', time2="03:24:12",
+                             openSeats='2', additionalDetails='None')
+        Event.objects.create(title='Shabbat', address='testaddress', city='Brooklyn', state='New York',
+                             phone='1231231234', date='2016-12-07', time1='03:24:12', time2="03:24:12",
+                             openSeats='10', additionalDetails='None')
+        response = self.client.get('/')
+        self.assertContains(response, 'Frijay')
+        self.assertContains(response, 'Queens')
+        self.assertContains(response, '2')

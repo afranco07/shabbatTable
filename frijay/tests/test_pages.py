21b1ffa3f9c385b36b1bee71f595d398a70a189c
Alberto Franco <afranco07@citymail.cuny.edu>
2016-12-08 10:35:55 -0500

    Made tests for the titles on each page when the user is not logged in

--- frijay/tests/test_pages.py
+++ frijay/tests/test_pages.py
@@ -25,3 +25,23 @@ class TestPages(TestCase):
         self.assertEquals(buttons[3].get_text(), 'How It Works')
         self.assertEquals(buttons[4].get_text(), ' Login')
         self.assertEquals(buttons[5].get_text(), ' Sign Up')
+
+    def test_homepageTitle(self):
+        html = urlopen('https://shabbattable.herokuapp.com/').read()
+        soup = BeautifulSoup(html, 'html.parser')
+        self.assertEquals(soup.title.get_text(), ' Frijay')
+
+    def test_eventTitle(self):
+        html = urlopen('https://shabbattable.herokuapp.com/events/').read()
+        soup = BeautifulSoup(html, 'html.parser')
+        self.assertEquals(soup.title.get_text(), ' Events Page')
+
+    def test_aboutTitle(self):
+        html = urlopen('https://shabbattable.herokuapp.com/about/').read()
+        soup = BeautifulSoup(html, 'html.parser')
+        self.assertEquals(soup.title.get_text(), '  About Us ')
+
+    def test_howWorksTitle(self):
+        html = urlopen('https://shabbattable.herokuapp.com/howitworks/').read()
+        soup = BeautifulSoup(html, 'html.parser')
+        self.assertEquals(soup.title.get_text(), ' How It Works')

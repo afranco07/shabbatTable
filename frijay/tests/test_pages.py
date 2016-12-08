46b8c2efcf0c7e82a0e4a0e810b9f1d5d5be1a32
Alberto Franco <afranco07@citymail.cuny.edu>
2016-12-08 13:39:07 -0500

    Merge issues

--- frijay/tests/test_pages.py
+++ frijay/tests/test_pages.py
@@ -6,10 +6,6 @@ from urllib.request import urlopen
 html = urlopen('https://shabbattable.herokuapp.com/').read()
 soup = BeautifulSoup(html, 'html.parser')
 
-
-
-
-
 class TestPages(TestCase):
 
     def test_navbar_title(self):
